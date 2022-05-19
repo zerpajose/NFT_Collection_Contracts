f429da8e20476ae676e2a51efbd3a929a37e8f1a
CryptoVincent <zjose88@gmail.com>
2022-05-19 04:57:20 -0400

    Update and fixes

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x595521da4cDE85f68F1875801d84f9c8FA8e057A";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://api.fluf.world/api/token";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
